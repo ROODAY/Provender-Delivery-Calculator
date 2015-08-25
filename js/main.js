2889c6c06b55d95600eec7e0b142923d6adbea52
Rudhra Raveendran <rudytheninja@gmail.com>
2015-08-25 14:39:17 -0400

    travel calculations work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,61 @@
+var keys = {37: 1, 38: 1, 39: 1, 40: 1, 32: 1, 33: 1, 34: 1, 35: 1, 36: 1};
 
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
+
+disableScroll();
+
+$(document).ready(function(){
+  var directionsDisplay, directionsService;
+  $("#google-map").css("height", $("#driving-stats").height() + "px");
+  var map = document.querySelector('google-map');
+  map.latitude = 37.77493;
+  map.longitude = -122.41942;
+  map.addEventListener('google-map-ready', function(e) {
+    directionsService = new google.maps.DirectionsService();
+    setTimeout(function(){
+		$("#loading").css("margin-top", "-100vh");
+    }, 300);
+    enableScroll();
+  });
+
+  function initialize() {
+	  directionsDisplay = new google.maps.DirectionsRenderer();
+	  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+	  var mapOptions = {
+	    zoom:7,
+	    center: chicago
+	  };
+	  map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+	  directionsDisplay.setMap(map);
+	}
+});
