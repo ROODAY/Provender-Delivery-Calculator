b099886aeab4e65e166ca59fd762a2e8f6fc7d43
Rudhra Raveendran <rudytheninja@gmail.com>
2015-08-26 16:28:48 -0400

    finished most of the calculations

--- js/main.js
+++ js/main.js
@@ -110,7 +110,100 @@ $(document).ready(function(){
       $(".form-input").val("");
     }
   }
-
-  Polymer.addEventListener(document.getElementById('nextPage'), 'tap', checkReset);
 });
 
+var destinationCount = 2,
+    taskCount = 4,
+    workerCount = 3,
+    expenseCount = 3;
+appendExpense = function() {
+  $("#expenses-inputs-container").append("<form-input info='{\"label\": \"Expense-" + expenseCount + "\"}'></form-input>");
+  expenseCount += 1;
+};
+appendWorker = function() {
+  $("#labor-inputs-container").append("<form-input info='{\"label\": \"Worker-" + workerCount + "\"}'></form-input>");
+  workerCount += 1;
+};
+appendTask = function() {
+  $("#tasks-inputs-container").append("<form-input info='{\"label\": \"Task-" + taskCount + "\"}'></form-input>");
+  taskCount += 1;
+};
+appendDestination = function() {
+  $("#driving-stats-inputs-container").append("<form-input info='{\"label\": \"Destination-" + destinationCount + "\"}'></form-input>");
+  destinationCount += 1;
+  $("#google-map").css("height", $("#driving-stats").height() + "px");
+};
+
+runCalculations = function() {
+  calcDrivingStats();
+  calcDepreciation();
+};
+
+calcDepreciation = function() {
+  var amount = parseInt($("#input-loan-amount").val()),
+      term = parseInt($("#input-term-of-loan-in-years").val()),
+      rate = parseInt($("#input-annual-interest-rate").val()),
+      periods = parseInt($("#input-payments-per-year").val());
+
+  return  {
+    ratePerPeriod: Math.round(((1 + (rate/100)/periods) - 1) * 10000) / 10000,
+    paymentPerPeriod: Math.round(((ratePerPeriod * amount) / (1 - (Math.pow((1 + ratePerPeriod), -1 * periods * term)))) * 100) / 100,
+    numPayments: Math.round(NPER(ratePerPeriod, paymentPerPeriod, -1 * amount)),
+    totalPayment: Math.round((paymentPerPeriod * numPayments) * 100) / 100,
+    totalInterest: Math.round((totalPayment - amount) * 100) / 100
+  };
+    
+};
+
+function NPER(rate, payment, present) {
+  var type = 0;
+  var future = 0;
+  var num = payment * (1 + rate * type) - future * rate;
+  var den = (present * rate + payment * (1 + rate * type));
+  return Math.log(num / den) / Math.log(1 + rate);
+}
+
+calcTaskTime = function() {
+  tasks = $("#tasks-inputs-container").find("input");
+  totalTime = 0;
+  for (var i = 0; i < tasks.length; i++) {
+    totalTime += parseInt($(tasks[i]).val());
+  }
+  return totalTime;
+};
+
+calcLaborCosts = function(hours) {
+  wages = $("#labor-inputs-container").find("input");
+  totalLaborCost = 0;
+  for (var i = 0; i < wages.length; i++) {
+    totalLaborCost += parseInt($(wages[i]).val()) * hours;
+  }
+  return totalLaborCost;
+};
+
+calcDrivingStats = function() {
+  var obj = $("#driving-stats-inputs-container").find("input");
+  var locations = [];
+  for (var i = 1; i < obj.length; i++) {
+    input = obj[i];
+    point = {
+      location: $(input).val(),
+      stopover: false
+    };
+    locations.push(point);
+  }
+  var req = {
+    origin: $(obj[0]).val(),
+    destination: $(obj[0]).val(),
+    waypoints: locations,
+    provideRouteAlternatives: false,
+    travelMode: google.maps.TravelMode.DRIVING,
+    unitSystem: google.maps.UnitSystem.IMPERIAL
+  };
+  metrics = [];
+  directionsService.route(req, function(res){
+    metrics.push(res.routes[0].legs[0].distance);
+    metrics.push(res.routes[0].legs[0].duration);
+    console.log(metrics);
+  });
+};
